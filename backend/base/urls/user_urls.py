46f143c54fbfd7cc4888bbbabcbf8965d79d3a71
devckrishna <dev.c.krishna.123@gmail.com>
2021-05-07 16:39:04 +0530

    some

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('profile/', views.getUserProfile, name="users-profile"),
+    path('profile/update/', views.updateUserProfile, name="users-profile-update"),
     path('', views.getUsers, name="users"),
     path('register/', views.registerUser, name="register-user"),
 ]
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,13 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer, productDetailsReducer } from "./reducers/ProductReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers"
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
