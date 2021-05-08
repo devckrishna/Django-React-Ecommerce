858debe487fe41f49afeabe944482a2ad214df9e
devckrishna <dev.c.krishna.123@gmail.com>
2021-05-08 18:06:40 +0530

    final try

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,13 +18,16 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # path('api/', include('base.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/products/', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
     path('api/orders/', include('base.urls.order_urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
