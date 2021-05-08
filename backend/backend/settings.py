ee1b3d3bf16ad6849586382a4585917cff273b9c
devckrishna <dev.c.krishna.123@gmail.com>
2021-05-08 18:29:16 +0530

    final some

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-20+sdv8akjakeus*^l@!0*=2i7pa@&3bdb@4(5m39%cenz1!@v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
